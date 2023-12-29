bc99d2654effff0a30274176d35dadf73d3bb641
Noyalbabu <20209749@hope.ac.uk>
2023-12-29 00:27:32 +0000

    created initial page for the news site

--- news_app/src/App.js
+++ news_app/src/App.js
@@ -1,30 +1,81 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import NewsCatalog from './module/NewsCatalog';
 import './App.css';
 
 function App() {
     const [showNews, setShowNews] = useState(false);
+    const [topic, setTopic] = useState();
+    const [hover, setHover] = useState(false);
+
+    let restColor = 'rgb(234, 243, 236)';
+    let hoverColor = 'rgb(255, 255, 255)';
+
     const handleClick = () => {
         setShowNews(!showNews);
     };
-    return (
-        <div className="news_app">
-            <div className='header'>
-                <h1 >NEWS</h1>
-                <h2 >Newer Efficient Way of Source</h2>
-            </div>
-            <div className='tool_bar'>
-                <button onClick={handleClick}><b>Explore</b></button> 
-                <button>Navigation</button>
-                <button>Search</button>
-                <button>Language</button>
-                <button>Profile</button>
-            </div>
-            {showNews && <NewsCatalog />}
 
-        </div>
-    )
-}
+    const handleTopic = (event) => {
+        var topic = event.target.value;
+        setTopic(topic);
+        console.log(topic);
+    };
+    useEffect(() => {},[hover]);
+        return (
+            <div className="news_app">
+                <div className='header'>
+                    <h1 >NEWS</h1>
+                    <h2 >Newer Efficient Way of Source</h2>
+                </div>
+                <div className='tool_bar'>
+                    <button style={{backgroundColor: hover === true ? hoverColor : restColor}}
+                            onMouseOver={(event) => {
+                                event.preventDefault();
+                                setHover(true);
+                            }}
+                            onMouseLeave={(event) => {
+                                event.preventDefault();
+                                setHover(false);
+                            }}
+                            onClick={handleClick}>Explore</button>
+                            
+                    <button style={{backgroundColor: hover === true ? hoverColor : restColor}}
+                            onMouseOver={(event) => {
+                                event.preventDefault();
+                                setHover(true);
+                            }}
+                            onMouseLeave={(event) => {
+                                event.preventDefault();
+                                setHover(false);
+                            }}>Navigation</button>
+                            
+                    <button style={{backgroundColor: hover === true ? hoverColor : restColor}}
+                            onMouseOver={(event) => {
+                                event.preventDefault();
+                                setHover(true);
+                            }}
+                            onMouseLeave={(event) => {
+                                event.preventDefault();
+                                setHover(false);
+                            }}>Language</button>
+                            
+                    <button style={{backgroundColor: hover === true ? hoverColor : restColor}}
+                            onMouseOver={(event) => {
+                                event.preventDefault();
+                                setHover(true);
+                            }}
+                            onMouseLeave={(event) => {
+                                event.preventDefault();
+                                setHover(false);
+                            }}>Profile</button>
+
+                    <input type="text" placeholder="Search.." name="topic" onChange={handleTopic}/>
+                </div>
+                {showNews && <NewsCatalog topic ={topic} />}
+            </div> 
+        )
+    }
+
+    
 /*As you see I used the useState hook to create a state variable called showNews and a function called setShowNews. 
 The button 'Explore' is used to toggle the news. 
 On clicking the button you can see the and hide the news.
--- news_app/src/module/NewsCatalog.js
+++ news_app/src/module/NewsCatalog.js
@@ -2,23 +2,25 @@ import React, {useState, useEffect} from "react";
 import NewsElement from "./NewsElement";
 import axios from "axios";
 
+
+
 //Declaring const for the news catalog
-const NewsCatalog = () => {
+const NewsCatalog = ({topic}) => {
     const [articles, setnewsArticle] = useState([]);
-    useEffect(() => {
-        const fetchnewsArticle = async () => {
-            try{
-            console.log("Fetching news data");
-            const response = await axios.get('https://newsapi.org/v2/everything?q=Nature&from=2023-12-24&sortBy=politics&apiKey=43d13539acbd4ba4814f3e166a9130d7');
-            setnewsArticle(response.data.articles);
-            //print the data 
-            console.log(response);
-            } catch (error) {
-                console.error("Error fetching news:", error);
-            }
-    };
-        fetchnewsArticle();
- }, [] );
+        useEffect(() => {
+            const fetchnewsArticle = async () => {
+                try{
+                console.log("Fetching news data:", topic);
+                const response = await axios.get(`https://newsapi.org/v2/everything?q=${topic}&from=2023-12-27&to=2023-12-27&sortBy=popularity&apiKey=43d13539acbd4ba4814f3e166a9130d7`);
+                setnewsArticle(response.data.articles);
+                //print the data 
+                console.log(response);
+                } catch (error) {
+                    console.error("Error fetching news:", error);
+                }
+        };
+            fetchnewsArticle();
+     }, [topic] );
 
     return (
         <div className="display">
@@ -28,7 +30,7 @@ const NewsCatalog = () => {
                         <NewsElement
                             key ={article.source.id}
                             title ={article.title}
-                            content ={article.content}
+                            description ={article.description}
                             url ={article.url}
                             urlToImage ={article.urlToImage} 
                             source ={article.source.name}
