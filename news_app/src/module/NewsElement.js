ca1730aecdc1b88bf4a38d0a5f4acff785389144
Noyal Babu <noyalbabu02@gmail.com>
2024-04-02 00:30:31 +0100

    Added comment box. Need to update css

--- news_app/src/module/NewsElement.js
+++ news_app/src/module/NewsElement.js
@@ -1,8 +1,29 @@
+import { ThemeConsumer } from 'styled-components';
 import './NewsElement.css';
-import React from 'react';
+import React, {useState} from 'react';
 
 // We are creating a component called NewsElement to display the title, descriptuion, url, image from news API
 const NewsElement = ({urlToImage, title, description, url,source, date}) => {
+    const pass = sessionStorage.getItem("token");
+    //GET HOLD OF THE COMMENT. 
+    //credict: https://www.youtube.com/watch?v=fJzxTYZLsy4&ab_channel=DeveloperGeorge
+    const [comment, setComment] = useState(''); 
+    const getComment = (event) => {
+        setComment(event.target.value);
+    }
+
+    //DISPLAY ALL THE COMMENTS USERS HAD MADE
+    const [displayComment, setDisplayComment] = useState([]);
+    const ondisplayComment = () => {
+        var commentBox = document.getElementById('comment-input');
+        if (commentBox.value === '') {
+            alert('Please enter a comment');
+        }
+        else{
+        setDisplayComment((displayComment) => [...displayComment, comment]);
+        //remove the text from the comment box after the user submit the comment
+        commentBox.value = '';
+    } }
     return (
         <div className="newsWebsite">
             <div className="newsElement">
@@ -12,6 +33,16 @@ const NewsElement = ({urlToImage, title, description, url,source, date}) => {
             <h3>Source:{source}</h3>
             <h3>Date: {date}</h3>
             <h3><a href={url}>Look Up for more Info!</a></h3>
+    
+            <div className="commentSection">
+            {displayComment.map((text) =>(
+                <div id='comment-display'>{text}</div>
+         ) )}
+            <h3 id='comment-title'>Comment</h3>
+            <textArea id='comment-input'
+            value={comment}
+            onChange={getComment} placeholder = {pass}/>
+            <button onClick={ondisplayComment} id='comment-sub'>Submit</button></div>
             </div>   
         </div>
     )
--- news_app/src/nav/account.js
+++ news_app/src/nav/account.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import { FaUserCog } from "react-icons/fa";
+
 
 function Account() {
     const [userData, setUserData] = useState(null);
@@ -22,14 +24,14 @@ function Account() {
     }, []);
 
     return (
-        <div className="profile">
-            <h1>Profile</h1>
+        <div className ="profile">
+            <FaUserCog size={100}/>
             {userData && (
                 <div>
-                    <p>Name: {userData.name}</p>
-                    <p>Username: {userData.username}</p>
-                    <p>Email: {userData.email}</p>
-                    <p>Age: {userData.age}</p>
+                    <p>Name:  {userData.name}</p>
+                    <p>Username:  {userData.username}</p>
+                    <p>Email:  {userData.email}</p>
+                    <p>Age:  {userData.age}</p>
                 </div>
             )}
         </div>
